c70e99bdeaa6cfa7ef2011721e938a5adaebcaa3
Faur Luciana <luciana_faur@yahoo.com>
2020-09-26 18:55:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+  double suma, perioada, procentDobanda;
+  double sumaDobanda, sumaTotala;
+  cout << "Introduceti suma imprumutata: ";
+  cin >> suma;
+  cout << "Introduceti perioada de returnare a sumei: ";
+  cin >> perioada;
+  cout << "Introduceti rata dobanzii anuale: ";
+  cin >> procentDobanda;
+  sumaDobanda = suma * procentDobanda /100 * perioada;
+  sumaTotala = suma + sumaDobanda;
+  cout << "Dobanda platita de " << procentDobanda << " % pe o perioada de " << perioada << " ani pentru o suma de " << suma << " este " << sumaDobanda << endl;
+  cout << "Deci suma totala care se va plati in " << perioada << " este de " << sumaTotala << endl;
+  cout << endl;
+  return 0;  
+}
+//DOBANDA COMPUSA 
